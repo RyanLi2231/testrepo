88940724d1fd7788c88b22f61135abb368016c7f
ShatterdPotato <saahmed5526@gmail.com>
2025-01-10 13:39:58 -0500

    Second partner added code

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,8 @@ public class Main {
         if (1 == 1) {
             System.out.println("AAAAAAAAAAAAAAAAA");
         }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
