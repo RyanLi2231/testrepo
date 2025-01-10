2907737109da4aeea1b6fde13a2ccf10ba3b5682
ShatterdPotato <saahmed5526@gmail.com>
2025-01-10 13:37:41 -0500

    Second partner added code

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("this is the other partner adding some code");
         System.out.println("Change 3");
+        System.out.println("Another print statement");
     }
 }
