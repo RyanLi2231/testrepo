ee1f1776a42a07fe824820bc45b77d9330a9bb5d
BT_1N3_21 <sample@bths.edu>
2025-01-10 13:45:20 -0500

    Class2 Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("this is the other partner adding some code");
+        System.out.println("Change String");
         System.out.println("Change 3");
         System.out.println("Another print statement");
         if (1 == 1) {
