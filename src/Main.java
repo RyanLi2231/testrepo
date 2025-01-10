70cd8b1c48bd55d3bd2df1619fe2ac721c114b1d
BT_1N3_21 <sample@bths.edu>
2025-01-10 13:34:11 -0500

    Initial

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("this is the other partner adding some code");
+        System.out.println("Change 3");
     }
 }
